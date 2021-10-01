e8ca3376223d4bc3b0202f50781375ab1e26bd19
RomanovAleksander <1eksandr999@gmail.com>
2021-10-01 14:24:46 +0300

    Updates

---
+++ precache-manifest.33681b52f121e0d2c30fb3e3931b3baf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d164ae8c6364f5184fe51debe5bfc448",
+    "url": "/Stopwatch/index.html"
+  },
+  {
+    "revision": "11f2c1a8b2a6fd54d7cd",
+    "url": "/Stopwatch/static/css/main.564030d4.chunk.css"
+  },
+  {
+    "revision": "eb635675a40166f01cd3",
+    "url": "/Stopwatch/static/js/2.237dbd34.chunk.js"
+  },
+  {
+    "revision": "11f2c1a8b2a6fd54d7cd",
+    "url": "/Stopwatch/static/js/main.9d3e07b6.chunk.js"
+  },
+  {
+    "revision": "734f9df28dd49d7892ce",
+    "url": "/Stopwatch/static/js/runtime~main.2de4a6b0.js"
+  }
+]);
